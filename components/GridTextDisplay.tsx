d0505360a1ec91fbbd3ca2f5c3fea1e384c182a4
lokigod69 <152258458+lokigod69@users.noreply.github.com>
2025-11-16 01:14:53 +0800

    textgrid

--- components/GridTextDisplay.tsx
+++ components/GridTextDisplay.tsx
@@ -92,7 +92,7 @@ export function GridTextDisplay({ album, visible }: GridTextDisplayProps) {
     <group 
       ref={groupRef}
       position={position}
-      rotation={[-Math.PI / 2, 0, 0]}  // Lay flat on grid (90° rotation on X-axis)
+      rotation={[-Math.PI / 3, 0, 0]}  // Lay flat on grid (90° rotation on X-axis)
     >
       {/* Main text layer - album dominant color */}
       <Text
